c0e1c7792998cf268f6d542acfb23be7468d1ad6
laptop <matt.v.hartley@gmail.com>
2018-04-17 09:21:27 +0100

    add sold sign functionality

--- resources/views/stock.blade.php
+++ resources/views/stock.blade.php
@@ -8,47 +8,53 @@
 
 @section('content')
 
-            <div class="content">
-                <div class="container">                  
-                  	<div class="title m-b-md">
-                        In Stock
-                   	</div>
+<div class="content">
+    <div class="container">                  
+        <div class="title m-b-md">
+            In Stock
+        </div>
 
-					<div class="row">
-                   	@foreach($info['boats'] as $boat)
+        <div class="row">
+            @foreach($info['boats'] as $boat)
 
-                   		<!--  Boat {{{ $boat['id'] }}} -->
-                   		
-                   			<div class="boatcard col-lg-5">
-                   				<div class="cardtitle">
-                   					 <h3>{{{ $boat->title }}}</h3>
-                   				</div>
-                          @if($boat->img->count()>0)
-                   				   <div class="cardimage" style="background-image:url('{{ url(Storage::url($boat->img->sortByDesc('primary')->first()->src)) }}')">
-                          @else
-                            <div class="cardimage" style="background-image:url('{{ url('/img/defaultBoat.png') }}')">
-                          @endif
-                   				</div>
-                   				<div class="cardfooter">
-                   					<p>{{{ $boat->shortsummary }}}</p>
-                   					<a href="{{{ url('/stock/detail/?target='.$boat->id) }}}" class="btn btn-info btn-lg centerBottomButton">
-                   						View more
-                   					</a>
-                   				</div>
-                   			
-	                   		</div>
+                <!--  Boat {{{ $boat['id'] }}} -->
 
+                <div class="boatcard col-lg-5">
+                    @if($boat->sold)
+                        <div class="soldBanner">SOLD</div>
+                    @endif
+                    <div class="cardtitle">
+                        <h3>{{{ $boat->title }}}</h3>
+                    </div>
 
-		               	@if($boat['id']%2 ==0)
-	    					</div>
+                @if($boat->img->count()>0)
+                    <div class="cardimage" style="background-image:url('{{ url(Storage::url($boat->img->sortByDesc('primary')->first()->src)) }}')"></div>
+                @else
+                    <div class="cardimage" style="background-image:url('{{ url('/img/defaultBoat.png') }}')"></div>
+                @endif
 
-	    					<div class="row">       	
-	                   	@endif
+                
+                <div class="cardfooter">
+                    <p>{{{ $boat->shortsummary }}}</p>
 
-                   	@endforeach
-                   	</div>	
-      
+                    <a href="{{{ url('/stock/detail/?target='.$boat->id) }}}" class="btn btn-info btn-lg centerBottomButton">
+                    	View more
+                    </a>
                 </div>
-            </div>
+
+                </div>
+
+
+                @if($boat['id']%2 ==0)
+                </div>
+
+                <div class="row">       	
+                @endif
+
+            @endforeach
+        </div>	
+
+    </div>
+</div>
 
 @endsection
