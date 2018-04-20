c32505973def3a6498f8ddb434d69d8354be1139
laptop <matt.v.hartley@gmail.com>
2018-04-20 14:15:49 +0100

    added link to enquriry manager on dashboard

--- resources/views/inside/dashboardHome.blade.php
+++ resources/views/inside/dashboardHome.blade.php
@@ -2,9 +2,14 @@
 
 @section('content')
 	<div class="container">
-		<a href="{{{ url('baseprice') }}}"><div class="col-sm-3 dashbutton">Edit Base Prices</div></a>
-		<a href="{{{ url('extras') }}}"><div class="col-sm-3 dashbutton">View Optional Extras</div></a>
-		<a href="{{{ url('faq-admin') }}}"><div class="col-sm-3 dashbutton">Manage FAQ</div></a>
-		<a href="{{{ url('stock-boat-admin') }}}"><div class="col-sm-3 dashbutton">Manage Stock Boats</div></a>
+		<div class="row">
+			<a href="{{{ url('baseprice') }}}"><div class="col-sm-3 dashbutton">Edit Base Prices</div></a>
+			<a href="{{{ url('extras') }}}"><div class="col-sm-3 dashbutton">View Optional Extras</div></a>
+			<a href="{{{ url('faq-admin') }}}"><div class="col-sm-3 dashbutton">Manage FAQ</div></a>
+			<a href="{{{ url('stock-boat-admin') }}}"><div class="col-sm-3 dashbutton">Manage Stock Boats</div></a>
+		</div>
+		<div class="row">
+			<a href="{{{ url('enquiries') }}}"><div class="col-sm-3 dashbutton">Manage Enquiries</div>
+		</div>
 	</div>
 @endsection
