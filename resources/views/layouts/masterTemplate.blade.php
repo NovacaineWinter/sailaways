380f9e6ac91d9836c75a58dea9ee5b51668a89b4
laptop <matt.v.hartley@gmail.com>
2018-04-25 15:09:08 +0100

    add facebook pixel

--- resources/views/layouts/masterTemplate.blade.php
+++ resources/views/layouts/masterTemplate.blade.php
@@ -12,6 +12,24 @@
 
               gtag('config', 'UA-116600911-1');
             </script>
+
+            <!-- Facebook Pixel Code -->
+            <script>
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '102414300625411');
+              fbq('track', 'PageView');
+            </script>
+            <noscript><img height="1" width="1" style="display:none"
+              src="https://www.facebook.com/tr?id=102414300625411&ev=PageView&noscript=1"
+            /></noscript>
+            <!-- End Facebook Pixel Code -->
         @endif
 
 
@@ -19,6 +37,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta property="siteurl" content="{{{url('/')}}}" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+
+
 
         <title>@yield('title')</title>
 
